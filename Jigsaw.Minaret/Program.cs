6adfc1f235e2321556c4db81e57dd960952d3ef0
Ahmed Eid <ahmedaeid@outlook.com>
2017-08-03 23:41:21 +0200

    Start self hosted NancyFx

--- Jigsaw.Minaret/Program.cs
+++ Jigsaw.Minaret/Program.cs
@@ -3,6 +3,7 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using Nancy.Hosting.Self;
 
 namespace Jigsaw.Minaret
 {
@@ -10,6 +11,26 @@ namespace Jigsaw.Minaret
     {
         static void Main(string[] args)
         {
+            StartNancyFxHost("1234");
+
+        }
+
+
+        private static void StartNancyFxHost(string port)
+        {
+            var config = new HostConfiguration
+            {
+                UrlReservations = new UrlReservations()
+                {
+                    CreateAutomatically = true
+                }
+            };
+            using (var host = new NancyHost(config, new Uri($"http://localhost:{port}")))
+            {
+                host.Start();
+                Console.WriteLine($"Running on http://localhost:{port}");
+                Console.ReadLine();
+            }
         }
     }
 }
