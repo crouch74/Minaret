63a29b68faf1e4b2ed5f1e25689dd56f55ebf72f
Ahmed Eid <ahmedaeid@outlook.com>
2017-08-04 12:00:32 +0200

    add endpoijnt to get members of a specific role

--- Jigsaw.Minaret/Modules/ClusterModule.cs
+++ Jigsaw.Minaret/Modules/ClusterModule.cs
@@ -1,20 +1,34 @@
-﻿using Akka.Actor;
+﻿using System.Linq;
+using System.Runtime.InteropServices;
+using Akka.Actor;
 using Akka.Cluster;
 using Jigsaw.Minaret.Actors.Messages;
 using Nancy;
 
 namespace Jigsaw.Minaret.Modules
 {
-    public class ClusterModule : NancyModule
+  public class ClusterModule : NancyModule
+  {
+    public ClusterModule() : base("api/cluster")
     {
-        public ClusterModule() : base("api/cluster")
+      Get["/", true] = async (_, ct) =>
         {
-            Get["/", true] = async(_, ct) =>
-            {
-                var res = await Program.ClusterManagerActor
-                    .Ask<ClusterEvent.CurrentClusterState>(new GetClusterStatusMessage());
-                return Response.AsJson(res);
-            };
-        }
+          var res = await GetCurrentClusterState();
+          return Response.AsJson(res);
+        };
+
+      Get["/{role}", true] = async (_, ct) =>
+        {
+          var state = await GetCurrentClusterState();
+          var res = state.Members.Where(m => m.Roles.Contains(_.role));
+          return Response.AsJson(res);
+        };
     }
+
+    private static async System.Threading.Tasks.Task<ClusterEvent.CurrentClusterState> GetCurrentClusterState()
+    {
+      return await Program.ClusterManagerActor
+                          .Ask<ClusterEvent.CurrentClusterState>(new GetClusterStatusMessage());
+    }
+  }
 }
